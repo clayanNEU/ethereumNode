559289f8113b63c047f624cd14017da7472ddf12
Clayton Yan <yan.cl@northeastern.edu>
2023-10-09 19:06:28 -0700

    comments

--- server/index.js
+++ server/index.js
@@ -10,12 +10,14 @@ const secp = require("ethereum-cryptography/secp256k1");
 app.use(cors());
 app.use(express.json());
 
+// balances that each address starts with, these are the public addresses
 const balances = {
   "0241e850d0f175166c90787a84beca53d11d23fe67ab748409d7fc483b68a3b174": 100,
   "027813b7ffadd16cbacbb0a1b7c5d5f28c07324ac4d57b3e77e784beb1d07465fe": 50,
   "03dea4d3b698a93c56d29614daae3f093fd829d9667f0e44d48b6f1f5e989b0cb5": 75,
 };
 
+// gets the balance from the address
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
   const balance = balances[address] || 0;
